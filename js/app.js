1b78f886b72822f3bc6d3f8f460b9121b3c8afea
zmcdaniel <zmcdaniel@gmail.com>
2016-09-16 11:11:30 -0700

    building out controller

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
 angular.module('GiphyApp', [])
 
-.run(function() {
-  console.log('App has loaded!');
-});
+    .run(function() {
+        console.log('App has loaded!');
+    })
+
+    .controller('GiphyCtrl', ['$scope', '$http', function($scope, $http) {
+
+        $scope.searchTerm = '';
+
+        $scope.search = function() {
+            var request = {
+                url: '',
+                method: 'GET',
+                params: {
+                    q: $scope.searchTerm
+                }
+            }
+            $http(request).then(function success(response) {
+                console.log(response);
+            }, function error(response) {
+                console.log(response);
+            });
+        };
+
+
+
+    }]);
